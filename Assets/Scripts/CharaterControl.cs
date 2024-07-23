8e696b06be92af8dd8ad57b39d7ac32042905912
Haridass-dev <haridass.artist@gmail.com>
2024-07-23 13:06:04 +0530

    jump animation

--- Assets/Scripts/CharaterControl.cs
+++ Assets/Scripts/CharaterControl.cs
@@ -1,23 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CharaterControl : MonoBehaviour
 {
 
     [SerializeField] float speed = 1f;
-    [SerializeField] float horizontalinput;
-    [SerializeField] Rigidbody2D rb;
+    private float horizontalinput;
+    private bool verticalinput;
+    Rigidbody2D rb;
+    SpriteRenderer sr;
     Vector2 xy;
+    [SerializeField] Vector2 showVelocity;
+    [SerializeField] float jump;
+
 
     
     void Start()
     {
-      rb = GetComponent<Rigidbody2D>();  
+      rb = GetComponentInChildren<Rigidbody2D>();  
+      sr = GetComponent<SpriteRenderer>();
     }
 
     
     void Update()
+    {
+        movement();
+        flipplayer();
+        jumpplayer();
+    }
+
+    void movement()
     {
         horizontalinput = Input.GetAxis("Horizontal");
         xy.Set(horizontalinput, 0);
@@ -25,8 +39,33 @@ public class CharaterControl : MonoBehaviour
         rb.velocity = xy * speed * Time.deltaTime;
     }
 
-    private void FixedUpdate()
+    void flipplayer()
     {
-        
+        if (horizontalinput < 0)
+        {
+            sr.flipX = true;
+        }
+        else
+        {
+            sr.flipX = false;
+
+        }
     }
+
+    void jumpplayer ()
+    {
+
+        verticalinput = Input.GetButton("Jump");
+
+        print(verticalinput);
+
+        if (verticalinput)
+        {
+            rb.AddForce(Vector2.up * jump);
+            GetComponent<Animator>().SetTrigger("jumped");
+        }
+
+        GetComponent<Animator>().SetTrigger("grounded");
+    }
+  
 }
